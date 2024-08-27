21f180cbfedbd11f6814d80d104672d25500828c
pauzuffi <paula.zuffinetti@gmail.com>
2024-08-27 11:09:53 -0300

    cierre menu navbar js

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const menuIcon = document.getElementById('menu-icon');
 const navbar = document.getElementById('navbar');
+const menuOptions = document.querySelectorAll("#navbar > a");
 
+/*este abre y cierra el iconito del menu*/
 menuIcon.addEventListener('click', () => {
     navbar.classList.toggle('active');
 });
+
+/*acá se cierra el menu por cada opción que toque*/
+for (let i = 0; i < menuOptions.length; i++) {
+    menuOptions[i].addEventListener('click', () => {
+        navbar.classList.toggle('active');
+    })
+}
